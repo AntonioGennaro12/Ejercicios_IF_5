7a57043b127a6cc080864703937fd73d4b8822e3
Antonio Gennaro <antgennaro@hotmail.com>
2023-04-16 22:04:43 -0300

    Ejercicios 5 parte 3 v1

--- ejercicios5.js
+++ ejercicios5.js
@@ -4,14 +4,20 @@ hasta 3 por cliente
 Preguntar e informar
 Parte #2: Se agrega pedir el tiempo 
 de paseo qu epuede ser de 30 a 60 minutos.
+Parte #3 se le agrega el precio 
+y la posibilida de pago vía la página feriaCoin. 
+Si paga por feriaCoin se informa el monto total a pagar.
+
 */
 
 const CANT_MAX_MASCOTAS   =   3;
 const PASEO_MINIMO_MIN    =  30;
 const PASEO_MAXIMO_MIN    =  60;
+const PRECIO_PASEO_PERRO  = 400;
+const PRECIO_PASEO_GATO   = 680;
 
-let cantPerros  = prompt("Cuantos Perros desea que paseemos?");
-let cantGatos   = prompt("Cuantos Gatos desea que paseemos?");
+let cantPerros  = prompt("Cuantos Perros desea que paseemos? El precio del paseo es de $"+PRECIO_PASEO_PERRO+".-");
+let cantGatos   = prompt("Cuantos Gatos desea que paseemos? El precio del paseo es de $"+PRECIO_PASEO_GATO+".-");
 let tiempoPaseo = prompt("Ingrese la duración del paseo que desea (en minutos)");
 
 /* Mejora para prevenir errores por no ingresar un número o presinar Enter*/
@@ -31,7 +37,8 @@ if((tiempoPaseo >= PASEO_MINIMO_MIN) && (tiempoPaseo <= PASEO_MAXIMO_MIN)) {
         if (cantPerros > 0) {document.write (cantPerros+ ".- Perro/s ")};
         if (cantGatos  > 0) {document.write (cantGatos+ ".- Gato/s ")};
         document.write(", por un tiempo de "+tiempoPaseo+"minutos. ");
-        }
+        document.write ("El precio total es de: $ " + 
+            (((cantPerros*PRECIO_PASEO_PERRO)+(cantGatos*PRECIO_PASEO_GATO))*1)+".-");        }
     else{ 
         document.write("Lo sentimos sólo podemos pasear hasta "+CANT_MAX_MASCOTAS+
          ".- perros ó gatos por cliente, en forma exclusiva.")
